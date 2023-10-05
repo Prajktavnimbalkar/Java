8bb1a67418d80a9afd142e6610c7fa3c809e7de0
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2023-10-05 13:33:59 +0530

    Add files via upload

---
+++ admission.java
@@ -0,0 +1,10 @@
+
+public class admission {
+	void admission(String name,int edu,int fee) {
+		System.out.println("admission susses");
+	}
+public static void main(String[] args) {
+	admission ad = new admission();
+	ad.admission("prajkta", 17, 1110);
+}
+}
