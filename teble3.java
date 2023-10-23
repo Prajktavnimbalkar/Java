3a7366938a5889ea40abba4be0b2aab9d0ee634d
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2023-10-23 17:12:52 +0530

    Add files via upload

---
+++ teble3.java
@@ -0,0 +1,14 @@
+package whileloop;
+
+public class teble3 {
+	public static void main(String[] args) {
+		int i = 3;
+		while (i<= 30) {
+			
+			System.out.println(i);
+			
+		 i = i+3;
+		}
+	}}
+
+
