6473cb64742aa5b27b51c60a7a54dba17e5a596a
Prajktavnimbalkar <147031604+Prajktavnimbalkar@users.noreply.github.com>
2023-10-25 11:09:16 +0530

    Add files via upload

---
+++ zeroseries.java
@@ -0,0 +1,18 @@
+package whileloop;
+
+public class zeroseries {
+	
+		public static void main(String[] args) {
+			int i = 1;
+			while (i<= 1000000) {
+			
+				System.out.println(i);
+			
+			 i = i*10;
+			}
+		}}
+
+		
+
+	
+
